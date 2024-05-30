f091bb423bcb30e66018fe79070949b83c73f5c9
Hakan Yusufoğlu <hyusufoglu@isbak.istanbul>
2024-05-30 14:03:51 +0300

    Update işlemleri için event yayınlama ve mongodbye kaydetme işlemleri gerçekleştirildi.

--- src/Product.Application/Controllers/ProductsController.cs
+++ src/Product.Application/Controllers/ProductsController.cs
@@ -6,36 +6,123 @@ using Shared.Services.Abstract;
 
 namespace Product.Application.Controllers
 {
-	public class ProductsController(IEventStoreService eventStoreService, IMongoDbService mongoDbService) : Controller
-	{
-		public async Task<IActionResult> Index()
-		{
-			var productCollection = mongoDbService.GetCollection<Shared.Models.Product>("Products");
-			var products = await (await productCollection.FindAsync(_ => true)).ToListAsync();
+    //Todo Business işlemleri şuan controller üzerinde yapılmaktadır. Bu işlemler bir servis katmanına taşınmalıdır. (best practise)
+    public class ProductsController(IEventStoreService eventStoreService, IMongoDbService mongoDbService) : Controller
+    {
+        public async Task<IActionResult> Index()
+        {
+            var productCollection = mongoDbService.GetCollection<Shared.Models.Product>("Products");
+            var products = await (await productCollection.FindAsync(_ => true)).ToListAsync();
 
-			return View(products);
-		}
+            return View(products);
+        }
 
-		//Ürün oluşturma sayfası
-		public IActionResult Create()
-		{
-			return View();
-		}
-		[HttpPost]
-		public async Task<IActionResult> Create(CreateProductVm model)
-		{
-			NewProductAddedEvent newProductAddedEvent = new()
-			{
-				ProductId = Guid.NewGuid().ToString(),
-				InitialCount = model.Count,
-				InitialPrice = model.Price,
-				IsAvailable = model.IsAvailable,
-				ProductName = model.ProductName
-			};
+        //Ürün oluşturma sayfası
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Create(CreateProductVm model)
+        {
+            NewProductAddedEvent newProductAddedEvent = new()
+            {
+                ProductId = Guid.NewGuid().ToString(),
+                InitialCount = model.Count,
+                InitialPrice = model.Price,
+                IsAvailable = model.IsAvailable,
+                ProductName = model.ProductName
+            };
 
-			await eventStoreService.AppendToStreamAsync("products-stream", new[] { eventStoreService.GenerateEventData(newProductAddedEvent) });
+            await eventStoreService.AppendToStreamAsync("products-stream", new[] { eventStoreService.GenerateEventData(newProductAddedEvent) });
 
-			return RedirectToAction("Index");
-		}
-	}
+            return RedirectToAction("Index");
+        }
+
+        public async Task<IActionResult> Edit(string productId)
+        {
+            var productCollection = mongoDbService.GetCollection<Shared.Models.Product>("Products");
+            var product = await (await productCollection.FindAsync(p => p.Id == productId)).FirstOrDefaultAsync();
+
+            return View(product);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CountUpdate(Shared.Models.Product model, int status)
+        {
+            var productCollection = mongoDbService.GetCollection<Shared.Models.Product>("Products");
+            var product = await (await productCollection.FindAsync(p => p.Id == model.Id)).FirstOrDefaultAsync();
+
+            if (status == 1)
+            {
+                CountDecreasedEvent countDecreasedEvent = new()
+                {
+                    ProductId = model.Id,
+                    DecrementAmount = model.Count,
+                };
+                await eventStoreService.AppendToStreamAsync("products-stream", new[] { eventStoreService.GenerateEventData(countDecreasedEvent) });
+            }
+            else if (status == 0)
+            {
+                CountIncreasedEvent countIncreasedEvent = new()
+                {
+                    ProductId = model.Id,
+                    IncrementAmount = model.Count,
+                };
+
+                await eventStoreService.AppendToStreamAsync("products-stream", new[] { eventStoreService.GenerateEventData(countIncreasedEvent) });
+            }
+            return RedirectToAction("Index");
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> PriceUpdate(Shared.Models.Product model, int status)
+        {
+            var productCollection = mongoDbService.GetCollection<Shared.Models.Product>("Products");
+            var product = await (await productCollection.FindAsync(p => p.Id == model.Id)).FirstOrDefaultAsync();
+
+            if (status==1)
+            {
+                PriceDecreasedEvent priceDecreasedEvent = new()
+                {
+                    ProductId = model.Id,
+                    DecrementAmount = model.Price,
+                };
+                await eventStoreService.AppendToStreamAsync("products-stream", new[] { eventStoreService.GenerateEventData(priceDecreasedEvent) });
+            }
+
+            else if (status == 0)
+            {
+                PriceIncreasedEvent priceIncreasedEvent = new()
+                {
+                    ProductId = model.Id,
+                    IncrementAmount = model.Price,
+                };
+
+                await eventStoreService.AppendToStreamAsync("products-stream", new[] { eventStoreService.GenerateEventData(priceIncreasedEvent) });
+            }
+
+            return RedirectToAction("Index");
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AvailableUpdate(Shared.Models.Product model)
+        {
+            var productCollection = mongoDbService.GetCollection<Shared.Models.Product>("Products");
+            var product = await (await productCollection.FindAsync(p => p.Id == model.Id)).FirstOrDefaultAsync();
+
+            if (product.IsAvailable != model.IsAvailable)
+            {
+                AvailabilityChangedEvent availabilityChangedEvent = new()
+                {
+                    ProductId = model.Id,
+                    IsAvailable = model.IsAvailable
+                };
+
+                await eventStoreService.AppendToStreamAsync("products-stream", new[] { eventStoreService.GenerateEventData(availabilityChangedEvent) });
+            }
+
+            return RedirectToAction("Index");
+        }
+    }
 }
--- src/Product.Event.Handler.Service/Services/EventStoreBackgroundService.cs
+++ src/Product.Event.Handler.Service/Services/EventStoreBackgroundService.cs
@@ -8,42 +8,89 @@ using System.Text.Json;
 
 namespace Product.Event.Handler.Service.Services
 {
-	public class EventStoreBackgroundService(IEventStoreService eventStoreService, IMongoDbService mongoDbService) : BackgroundService
-	{
-		protected override async Task ExecuteAsync(CancellationToken stoppingToken)
-		{
-			await eventStoreService.SubscribeToStreamAsync("products-stream", async (streamSubscription, resolvedEvent, cancellationToken) =>
-			{
-				//hangi event geldiyse ilgili event'e göre işlem yapılacak
-				string eventType = resolvedEvent.Event.EventType;
+    public class EventStoreBackgroundService(IEventStoreService eventStoreService, IMongoDbService mongoDbService) : BackgroundService
+    {
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            await eventStoreService.SubscribeToStreamAsync("products-stream", async (streamSubscription, resolvedEvent, cancellationToken) =>
+            {
+                //hangi event geldiyse ilgili event'e göre işlem yapılacak
+                string eventType = resolvedEvent.Event.EventType;
 
-				//shared altındaki bir türle dönüşüm yapıyoruz.
-				object @event=JsonSerializer.Deserialize(resolvedEvent.Event.Data.ToArray(),Assembly.Load("Shared").GetTypes().FirstOrDefault(t=>t.Name == eventType));
+                //shared altındaki bir türle dönüşüm yapıyoruz.
+                object @event = JsonSerializer.Deserialize(resolvedEvent.Event.Data.ToArray(), Assembly.Load("Shared").GetTypes().FirstOrDefault(t => t.Name == eventType));
 
 
-				var productCollection = mongoDbService.GetCollection<Shared.Models.Product>("Products");
+                var productCollection = mongoDbService.GetCollection<Shared.Models.Product>("Products");
 
-				switch (@event)
-				{
-					case NewProductAddedEvent e:
-						//MongoDb işlemleri yapılacak
-						//Daha önce oluşturduğumuz product var mı yok mu kontrol ediyoruz
+                switch (@event)
+                {
+                    case NewProductAddedEvent e:
+                        //MongoDb işlemleri yapılacak
+                        //Daha önce oluşturduğumuz product var mı yok mu kontrol ediyoruz
 
-						var hasProduct = await (await productCollection.FindAsync(p=>p.Id==e.ProductId)).AnyAsync();
+                        var hasProduct = await (await productCollection.FindAsync(p => p.Id == e.ProductId)).AnyAsync();
+                        Shared.Models.Product? product = null;
 
-						if (!hasProduct)
-							await productCollection.InsertOneAsync(new()
-							{
-								Id=e.ProductId,
-								ProductName = e.ProductName,
-								Count = e.InitialCount,
-								IsAvailable = e.IsAvailable,
-								Price = e.InitialPrice
-							});
+                        if (!hasProduct)
+                            await productCollection.InsertOneAsync(new()
+                            {
+                                Id = e.ProductId,
+                                ProductName = e.ProductName,
+                                Count = e.InitialCount,
+                                IsAvailable = e.IsAvailable,
+                                Price = e.InitialPrice
+                            });
 
-						break;
-				}
-			});
-		}
-	}
+                        break;
+
+                    //Todo: best practise açıdan aşağıdaki kod tekrarları engellenmeli
+                    case CountDecreasedEvent e:
+                        product = await (await productCollection.FindAsync(p => p.Id == e.ProductId)).FirstOrDefaultAsync();
+                        if (product != null)
+                        {
+                            product.Count -= e.DecrementAmount;
+                            await productCollection.FindOneAndReplaceAsync(p => p.Id == e.ProductId, product);
+                        }
+
+                        break;
+                    case CountIncreasedEvent e:
+                        product = await (await productCollection.FindAsync(p => p.Id == e.ProductId)).FirstOrDefaultAsync();
+                        if (product != null)
+                        {
+                            product.Count += e.IncrementAmount;
+                            await productCollection.FindOneAndReplaceAsync(p => p.Id == e.ProductId, product);
+                        }
+                        break;
+                    case PriceDecreasedEvent e:
+
+                        product = await (await productCollection.FindAsync(p => p.Id == e.ProductId)).FirstOrDefaultAsync();
+                        if (product != null)
+                        {
+                            product.Price -= e.DecrementAmount;
+                            await productCollection.FindOneAndReplaceAsync(p => p.Id == e.ProductId, product);
+                        }
+                        break;
+                    case PriceIncreasedEvent e:
+
+                        product = await (await productCollection.FindAsync(p => p.Id == e.ProductId)).FirstOrDefaultAsync();
+                        if (product != null)
+                        {
+                            product.Price += e.IncrementAmount;
+                            await productCollection.FindOneAndReplaceAsync(p => p.Id == e.ProductId, product);
+                        }
+                        break;
+                    case AvailabilityChangedEvent e:
+
+                        product = await (await productCollection.FindAsync(p => p.Id == e.ProductId)).FirstOrDefaultAsync();
+                        if (product != null)
+                        {
+                            product.IsAvailable = e.IsAvailable;
+                            await productCollection.FindOneAndReplaceAsync(p => p.Id == e.ProductId, product);
+                        }
+                        break;
+                }
+            });
+        }
+    }
 }
